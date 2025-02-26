2ec80f951ab00f570c5311e76a09033999e92b9c
user7210unix <user7210@proton.me>
2025-02-26 10:03:48 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 
-# Default directories
 SOURCES_BASE_DIR = os.getenv('LXPKG_SOURCES_BASE_DIR', '/home/user/sources')
 BUILD_DIR = os.getenv('LXPKG_BUILD_DIR', '/home/user/sources/build')
 INSTALL_DIR = os.getenv('LXPKG_INSTALL_DIR', '/usr')
 
-# Logging configuration
 LOG_DIR = os.getenv('LXPKG_LOG_DIR', '/var/log/lxpkg')
 
